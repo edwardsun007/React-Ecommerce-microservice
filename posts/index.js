8ba79658bcfcb48851631e8aa68a65af6295c6ae
edward sun <edwardsun007@gmail.com>
2024-01-08 13:51:39 -0800

    Create Post and Get Post API

--- posts/index.js
+++ posts/index.js
@@ -3,14 +3,15 @@ standard express app
 */
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const  { randomBytes } = require('crypto');
+
 const app = express();
+app.use(bodyParser.json());
 
 
 // for simplicity, all posts will be stored in memory
 const posts = {};
-
-
 
 app.get('/posts', (req, res) => {
     res.send(posts);
@@ -18,6 +19,15 @@ app.get('/posts', (req, res) => {
 
 app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex')
+    console.log('check id=', id);
+    console.log(req.body);
+    const {title} = req.body;
+
+    posts[id] = {
+        id,title
+    };
+    // express send back res with status code
+    res.status(201).send(posts[id])
 });
 
 app.listen(4000, ()=>{
