4003031b92c645caf1ae50d950b0e2392a0a95ac
edward sun <edwardsun007@gmail.com>
2024-01-13 00:42:09 -0800

    Comment Create Component

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function CommentCreate(props) {
+    const {postId} = props; // destruct it inside
+    const [commentContent, setCommentContent] = useState('');
+
+    const onSubmitHandler = async(event) => {
+        event.preventDefault();
+        await axios.post(
+            `'http://localhost:4001/posts/${postId}/comments'`,
+            {
+                content: commentContent
+            });
+        setCommentContent('');
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmitHandler}>
+                <div className='form-group'>
+                    <label>New Comment</label>
+                    <input value={commentContent} className='form-control' onChange={e=>setCommentContent(e.target.value)}/>
+                    {/* using value={commentContent} in your input field makes your form input a controlled component in React, 
+                    which gives you more control and predictability over the form's behavior and the value of its inputs. */}
+                </div>
+                <button className='btn btn-primary'>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default CommentCreate
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
 
 /* 
 component to fetch list of posts
@@ -39,6 +40,7 @@ function PostList() {
                         >
                             <div className='card-body'>
                                 <h3>{post.title}</h3>
+                                <CommentCreate postId={post.id} key={post.id}/>
                             </div>    
                         </div>
                 )}
