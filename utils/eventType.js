c44384d0e1ce053d89583cf111ec2e03aa7b62b1
edward sun <edwardsun007@gmail.com>
2024-01-31 13:53:21 -0800

    eventBus type module

---
+++ utils/eventType.js
@@ -0,0 +1,6 @@
+const Types = Object.freeze({
+    PostCreate: 'PostCreate',
+    CommentCreate:'CommentCreate'
+});
+
+module.exports = {Types}; // nodeJs by default expecting common JS syntax module, do need to use module.exports instead of exports
