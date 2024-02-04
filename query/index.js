72f13ab2793c10fbf6d2804be639ba88cd73d326
edward sun <edwardsun007@gmail.com>
2024-02-03 23:30:39 -0800

    query service implementing logic on handling events stream from event-bus

--- query/index.js
+++ query/index.js
@@ -6,19 +6,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const {Types} = require('../utils/eventType'); // common JS syntax of importing module using require
 
 const app = new express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res)=>{
+/*
+posts is object that is going to be structured as below
+// posts = {
+//     'postid': {
+//         id: 'postId',
+//         title: 'post title',
+//         comments:[
+//             { id: 'comment_id', content: 'comment1'},
+//             { id: 'comment_id', content: 'comment2'}
+//         ]
+//     }
+// }
+*/
+const posts = {}; 
 
+
+app.get('/posts', (req, res)=>{
+    app.send(posts);
 })
 
 // receive events from our event-bus
 app.post('/events', (req, res)=>{
-    
+    const {type, data} = req.body;
+    if(type === Types.PostCreate){
+        const { id, title } = data;
+        posts[id] = {id, title, comments: []} // store this post in the object
+
+    }
+
+    if(type === Types.CommentCreate){
+        const {id, content, postId} = data;
+        posts[postId].comments.push({id, content});
+    }
+
+    console.log('query checking posts:')
+    console.log(posts);
+    res.send({}); // since the object has been updated in posts, we dont need to send back anything
 });
 
 app.listen(4002, ()=>{
