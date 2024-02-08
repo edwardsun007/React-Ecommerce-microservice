c7ba5a5d8c16486ebbf1382d0c87f1d545072ae7
edward sun <edwardsun007@gmail.com>
2024-02-08 13:26:31 -0800

    React side Moderation Effect on comment

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import ModerationStatus from './ModerationTypes';
 
 function CommentList(props) {
     const {comments} = props; // destruct it inside
 
     const renderComments = comments.map(
         comment=>{
-            return <li key={comment.id}>{comment.content}</li>
+            let content;
+            if(comment.status === ModerationStatus.APPROVED){
+                content = comment.content;
+            }
+            if(comment.status === ModerationStatus.REJECTED){
+                content = 'Your content is removed! Find out why.'
+            }
+            if(comment.status === ModerationStatus.PENDING){
+                content ='Content is being moderated'
+            }
+            return <li key={comment.id}>{ content }</li>
         }
     )
     return (
---
+++ client/src/ModerationTypes.js
@@ -0,0 +1,8 @@
+const ModerationStatus = Object.freeze({
+    PENDING: 'PENDING',
+    APPROVED:'APPROVED',
+    REJECTED:'REJECTED',
+    MODERATED: 'MODERATED'
+});
+
+export default ModerationStatus;
