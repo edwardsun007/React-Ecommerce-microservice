1fe964e63d08feccd915f7a370bcefe304583528
edward sun <edwardsun007@gmail.com>
2024-02-08 14:05:03 -0800

    refactor event handling into helper function

--- query/index.js
+++ query/index.js
@@ -6,6 +6,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 const {EventTypes} = require('../utils/eventType'); // common JS syntax of importing module using require
 
 const app = new express();
@@ -28,14 +29,7 @@ posts is object that is going to be structured as below
 const posts = {}; 
 
 
-app.get('/posts', (req, res)=>{
-    console.log('query service sending posts now...');
-    res.send(posts);
-})
-
-// receive events from our event-bus
-app.post('/events', (req, res)=>{
-    const {type, data} = req.body;
+const handleEventHelper = (type, data) => {
     if(type === EventTypes.PostCreate){
         const { id, title } = data;
         posts[id] = {id, title, comments: []} // store this post in the object
@@ -57,12 +51,21 @@ app.post('/events', (req, res)=>{
         comment.status = status; // update its status
         comment.content = content; // we cannot be sure if there are other changes happened to comment content, so just update it as well
     }
+}
 
-    console.log('query checking posts:')
-    console.log(posts);
+app.get('/posts', (req, res)=>{
+    console.log('query service sending posts now...');
+    res.send(posts);
+})
+
+// receive events from our event-bus
+app.post('/events', (req, res)=>{
+    const {type, data} = req.body;
+
+    handleEventHelper(type, data);
     res.send({}); // since the object has been updated in posts, we dont need to send back anything
 });
 
-app.listen(4002, ()=>{
+app.listen(4002, ()=> {
     console.log('Query service listening on 4002');
 });
