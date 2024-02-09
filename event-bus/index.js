dedd36d038c50028cfb62adb3b3cec671afb46a2
edward sun <edwardsun007@gmail.com>
2024-02-08 23:33:54 -0800

    Add catch block since it is Node v15+ to avoid crashes

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,10 +13,19 @@ app.post('/events', (req, res)=>{
     events.push(event);
 
     // whenever there is event received, we will then make a series of events call to posts, comments
-    axios.post('http://localhost:4000/events', event);  // Post service
-    axios.post('http://localhost:4001/events', event);  // Comment service
-    axios.post('http://localhost:4002/events', event);  // Query Service
-    axios.post('http://localhost:4003/events', event);  // Moderation Service
+    // the following catch chaining is added due to breaking change of Node v15: https://nodejs.medium.com/node-js-v15-0-0-is-here-deb00750f278
+    axios.post('http://localhost:4000/events', event).catch((err)=>{
+        console.log(err);
+    });  // Post service
+    axios.post('http://localhost:4001/events', event).catch((err)=>{
+        console.log(err);
+    });  // Comment service
+    axios.post('http://localhost:4002/events', event).catch((err)=>{
+        console.log(err);
+    });  // Query Service
+    axios.post('http://localhost:4003/events', event).catch((err)=>{
+        console.log(err);
+    });  // Moderation Service
     // this code has flaw, currently it is just assuming all the above call can succeed
     res.send( {status: 'OK'}); 
 })
@@ -24,7 +33,7 @@ app.post('/events', (req, res)=>{
 // NOT really production scenario, simply a concept 
 app.get('/events', (req,res)=>{
     res.send(events);
-})
+});
 
 app.listen(4005, ()=>{
     console.log('Event-Bus listening on 4005');
